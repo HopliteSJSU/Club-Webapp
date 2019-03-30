7c15667e703bad9b49c8edcd2911b18030d09987
David Janda <jandad97@gmail.com>
2019-03-30 01:20:22 -0700

    Uniquely identify files using a userID sent with POST/GET requests.

--- backend/routes/media.js
+++ backend/routes/media.js
@@ -22,11 +22,12 @@ const checkFileType = (fileName) => {
 
 router.post('/api/update/files', (req, res) => {
     const fileName = req.body.file;
+    const userID = req.body.userID;
 
     if (checkFileType(fileName)) {
         s3.upload({
             Bucket: keys.bucket,
-            Key: fileName,
+            Key: userID + '-' + fileName,
             Body: req,
             ACL: 'public-read'
         }, (err, data) => {
@@ -35,25 +36,21 @@ router.post('/api/update/files', (req, res) => {
             console.log('File successfully uploaded.');
         });
     } else {
-        res.send('Unsupported file type. Please upload either a .jpg or .png file for images and either a .pdf, .docx, or .doc file for resumes.');
+        res.send('Unsupported file type. Please select either a .jpg or .png file for images and either a .pdf, .docx, or .doc file for resumes.');
     }
 });
 
 router.get('/api/files', (req, res) => {
-    const fileName = req.query.file;
+    const key = req.query.key; // Send GET request with value of the form: userID-fileName
 
-    if (checkFileType(fileName)) {
-        s3.getObject({
-            Bucket: keys.bucket,
-            Key: fileName
-        }, (err, data) => {
-            if (err) throw err;
-            else res.send(data);
-            console.log('File successfully retrieved.');
-        });
-    } else {
-        res.send('Unsupported file type. Please upload either a .jpg or .png file for images and either a .pdf, .docx, or .doc file for resumes.');
-    }
+    s3.getObject({
+        Bucket: keys.bucket,
+        Key: key
+    }, (err, data) => {
+        if (err) throw err;
+        else res.send(data);
+        console.log('File successfully retrieved.');
+    });
 });
 
 module.exports = router;
