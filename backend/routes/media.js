0d0b431e730f437080abe98226718e850e100a4e
David Janda <jandad97@gmail.com>
2019-03-30 02:13:32 -0700

    Simplified S3 key value by using the form userID-fileType

--- backend/routes/media.js
+++ backend/routes/media.js
@@ -11,23 +11,34 @@ const s3 = new AWS.S3({
 });
 
 const supportedFileTypes = {
-    image: ['jpg', 'png'],
+    picture: ['jpg', 'png'],
     resume: ['pdf', 'docx', 'doc']
 };
 
-const checkFileType = (fileName) => {
+const isValidFileType = (fileName) => {
     const fileType = fileName.split('.').pop();
-    return (supportedFileTypes.image.includes(fileType) || supportedFileTypes.resume.includes(fileType));
+    return (supportedFileTypes.picture.includes(fileType) || supportedFileTypes.resume.includes(fileType));
+};
+
+const getFileType = (fileName) => {
+    const fileType = fileName.split('.').pop();
+
+    if (supportedFileTypes.picture.includes(fileType)) {
+        return 'picture';
+    } else if (supportedFileTypes.resume.includes(fileType)) {
+        return 'resume';
+    }
 };
 
 router.post('/api/update/files', (req, res) => {
     const fileName = req.body.file;
+    const fileType = getFileType(fileName);
     const userID = req.body.userID;
 
-    if (checkFileType(fileName)) {
+    if (isValidFileType(fileName)) {
         s3.upload({
             Bucket: keys.bucket,
-            Key: userID + '-' + fileName,
+            Key: userID + '-' + fileType,
             Body: req,
             ACL: 'public-read'
         }, (err, data) => {
@@ -41,7 +52,7 @@ router.post('/api/update/files', (req, res) => {
 });
 
 router.get('/api/files', (req, res) => {
-    const key = req.query.key; // Send GET request with value of the form: userID-fileName
+    const key = req.query.key; // Send GET request with value of the form: userID-fileType where fileType can be either 'picture' or 'resume'
 
     s3.getObject({
         Bucket: keys.bucket,
