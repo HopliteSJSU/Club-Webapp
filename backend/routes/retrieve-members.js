5dc5519a3885063f064a9b855220e5f4705656b4
David Janda <jandad97@gmail.com>
2019-05-02 16:40:30 -0700

    Added member retrieval route

---
+++ backend/routes/retrieve-members.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const Member = require('../models/member');
+const router = express.Router();
+
+router.get('/retrieve/members', (req, res) => {
+  Member.find({}, (err, data) => {
+    console.log('Retrieving members from database...');
+    console.log(data);
+  });
+  res.send(data);
+});
+
+module.exports = router;
