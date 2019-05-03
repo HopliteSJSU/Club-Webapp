f3b2a0f7dd17cb83238616977ac36c8e89a79c68
taarushv <saikarthiktaarush.vemulapalli@sjsu.edu>
2019-05-03 04:28:05 -0700

    Member auth

--- backend/routes/media.js
+++ backend/routes/media.js
@@ -1,7 +1,8 @@
 const AWS = require('aws-sdk');
 const express = require('express');
 const keys = require('../config/keys');
-
+var request = require('request');
+const url = 'https://www.googleapis.com/oauth2/v1/tokeninfo?access_token='
 const router = express.Router();
 
 const s3 = new AWS.S3({
@@ -26,26 +27,36 @@ const getFileType = (fileName) => {
 
     return false;
 };
-
+// Protected route
 router.post('/api/update/files', (req, res) => {
     const fileName = req.body.file;
-    const userID = req.body.userID;
-    const fileType = getFileType(fileName);
-
-    if (fileType) {
-        s3.upload({
-            Bucket: keys.bucket,
-            Key: userID + '-' + fileType,
-            Body: req,
-            ACL: 'public-read'
-        }, (err, data) => {
-            if (err) throw err;
-            else res.send(data);
-            console.log('File successfully uploaded.');
-        });
-    } else {
-        res.send('Unsupported file type. Please select either a .jpg or .png file for images and either a .pdf, .docx, or .doc file for resumes.');
-    }
+    // Retrieve access token from request
+    const token = req.body.token;
+    // Validate token before uploading
+    request.get(url+token, (err, res, body)=>{
+        // Verify email
+        if(JSON.parse(body)["verified_email"]===true && JSON.parse(body).email.split("@").pop() === 'sjsu.edu'){
+            const userID = body.email;
+            const fileType = getFileType(fileName);
+            if (fileType) {
+                s3.upload({
+                    Bucket: keys.bucket,
+                    Key: userID + '-' + fileType,
+                    Body: req,
+                    ACL: 'public-read'
+                }, (err, data) => {
+                    if (err) throw err;
+                    else res.send(data);
+                    console.log('File successfully uploaded.');
+                });
+            } else {
+                res.send('Unsupported file type. Please select either a .jpg or .png file for images and either a .pdf, .docx, or .doc file for resumes.');
+            }
+        }else{
+            res.send('Invalid Token, Please Re-login')
+        }
+        
+    })
 });
 
 router.get('/api/files', (req, res) => {
--- frontend/src/components/member-portal/member-portal.js
+++ frontend/src/components/member-portal/member-portal.js
@@ -1,8 +1,9 @@
 import React from "react";
-
+import GoogleLogin from 'react-google-login'
 import Button from "components/button/button";
 import CheckIn from "components/check-in/check-in";
 import Login from "components/log-in/log-in";
+import Cookies from 'universal-cookie';
 
 let applyLink = "https://goo.gl/forms/1KcEXaY9r4dA2mGi1";
 
@@ -12,12 +13,31 @@ class MemberPortal extends React.Component {
     showCheckIn: false,
     showLogin: false
   };
-
+  responseGoogle = (res)=>{
+    console.log(res)
+  }
   handleLogin = () =>
     this.setState({ showLogin: true, showDefault: false });
 
   handleCheckIn = () =>
     this.setState({ showCheckIn: true, showDefault: false });
+
+  handleLoginFail = () => 
+    alert("Login failed, Make sure to login with sjsu.edu");
+
+  handleLoginSuccess = (res) => {
+    const email = res.profileObj.email
+    if(email.split("@").pop() === 'sjsu.edu'){
+      const accessToken = res.accessToken;
+      const cookies = new Cookies();
+      // Removing existing access tokens
+      cookies.remove('gToken')
+      // Add access token to cookies for further use
+      cookies.set('gToken', accessToken, { path: '/' });
+    }else{
+      alert("Invalid email, Only sjsu.edu emails supported")
+    }
+  }
 
   render() {
     const { showDefault, showCheckIn, showLogin } = this.state;
@@ -42,11 +62,17 @@ class MemberPortal extends React.Component {
                     label="Meeting Check-In"
                     clicked={this.handleCheckIn}
                   />
-                  <Button
-                    label="Member Log-in"
-                    clicked={this.handleLogin}
-                  />
+                  <GoogleLogin
+    clientId="129086773964-42vg3lj1qos4j24nc31nv1mfj34s7m20.apps.googleusercontent.com"
+    onSuccess={this.handleLoginSuccess}
+    onFailure={this.handleLoginFail}
+    cookiePolicy={'single_host_origin'}
+    render={renderProps => (
+      <button onClick={renderProps.onClick} disabled={renderProps.disabled} className="button">Member-login</button>
+    )}
+  />
                 </div>
+                
               </React.Fragment>
             )}
             {showCheckIn && <CheckIn />}
